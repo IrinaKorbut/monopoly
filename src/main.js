f97da511dd1ea2ec269765ecdb1716f31ede0be7
IrinaKorbut <irisharomka96@gmail.com>
2021-01-11 21:26:26 +0300

    feat: implement move from dice

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import movePlayer from './move_player/movePlayerFn';
-import { diceInitTest } from './dice/dice';
+import diceInit from './dice/dice';
+// import cells from './cells/cells';
 
 import './style/style.scss';
 import './style/dice.scss';
 // import './style/cell.scss';
 
 import './style/field.scss';
+import './style/player.scss';
 
-diceInitTest();
-movePlayer(25);
+diceInit();
--- src/move_player/movePlayerFn.js
+++ src/move_player/movePlayerFn.js
@@ -1,36 +1,38 @@
-const cells = [];
-
-function createArrOfCells() {
-  for (let i = 0; i < 40; i += 1) {
-    cells.push(document.getElementById(`${i}`));
-  }
-}
+import cells from '../cells/cells';
 
 function checkWherePlayerNow(player) {
   const currentPlayerCell = player.parentNode;
   currentPlayerCell.removeChild(player);
-  const currentPlayerPosition = cells.indexOf(currentPlayerCell);
+  let currentPlayerPosition;
+  cells.forEach((item) => {    
+    if (item.element === currentPlayerCell) {
+      currentPlayerPosition = item.position;
+    };
+  });
   return currentPlayerPosition;
 }
 
 function showAnimationMove(currentPlayerPosition, playerPositionAfterMove, player) {
   for (let i = currentPlayerPosition + 1, j = 1; i <= playerPositionAfterMove; i += 1) {
+    if (i === 40) {
+      playerPositionAfterMove = playerPositionAfterMove - 40;
+      i = 0;
+    }
+
     if (i < playerPositionAfterMove) {
-      setTimeout(() => { cells[i].classList.toggle('active'); }, j * 400);
-      setTimeout(() => { cells[i].classList.toggle('active'); }, j * 400 + 200);
+      setTimeout(() => { cells[i].element.classList.toggle('active'); }, j * 400);
+      setTimeout(() => { cells[i].element.classList.toggle('active'); }, j * 400 + 200);
       j += 1;
-    }
+    };
     if (i === playerPositionAfterMove) {
-      setTimeout(() => { cells[playerPositionAfterMove].appendChild(player); }, j * 400);
-    }
-  }
-}
+      setTimeout(() => { cells[playerPositionAfterMove].element.appendChild(player); }, j * 400);
+    };
+  };
+};
 
 export default function movePlayer(stepsAmount) {
-  createArrOfCells();
   const player = document.querySelector('.player');
   const currentPlayerPosition = checkWherePlayerNow(player);
   const playerPositionAfterMove = currentPlayerPosition + stepsAmount;
   showAnimationMove(currentPlayerPosition, playerPositionAfterMove, player);
-//   setTimeout(() => { cells[playerPositionAfterMove].appendChild(player); }, 6000);
 }
