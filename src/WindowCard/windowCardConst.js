3834385e08c95db055c4ab1dcf367cd2fffdf061
Ivan Gawr <ivangawr1993>
2021-01-11 23:53:15 +0300

    feat: add const functions

--- src/WindowCard/windowCardConst.js
+++ src/WindowCard/windowCardConst.js
@@ -1,5 +1,27 @@
 import { removeCardStreet } from './windowCard';
 
+export function createElementAdd(elementType, elementsClassList, elementInnerText) {
+    const element = document.createElement(elementType);
+    element.classList.add(elementsClassList);
+    if (elementInnerText) {
+        element.innerText = elementInnerText;
+    }
+    return element;
+}
+
+export function createCell(className, divText, cellText) {
+    const cell = createElementAdd('div', className, divText);
+    const cellInnerText = createElementAdd('p', 'cost', cellText);
+    appendElementTo(cell, cellInnerText);
+    return cell;
+}
+
+export function appendElementTo(parent, ...elements) {
+    if (parent) {
+        elements.forEach((element) => parent.append(element));
+    }
+}
+
 export function addSelectorClass(classNames, fuNames) {
     let street = document.querySelectorAll(`.${classNames}`)
     for (let i = 0; i < street.length; i++) {
