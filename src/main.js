e9065d15f361afe142bfac5fddc3dd4af5060746
Sergey Danchenko <s.danchienko@mail.ru>
2021-01-20 19:12:25 +0300

    feat: create dice with canvas

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import { initWindowCards } from './WindowCard/windowCard';
-import showDialogWindow from './dialogWindow/dialogWindow';
-import diceInit from './dice/dice';
-import Game from './Game/Game';
-import Player from './Player/Player';
+import roll from './dice/dice';
 import { startWindow } from './StartWindow/startWindow';
 // import cells from './cells/cells';
-
-import { setNextPlayerAsActive } from './dialogWindow/dialogWindow'
 
 import renderPlayerCard from './playerCards/renderPlayerCard';
 import addPlayerToField from './addPlayerToField/addPlayerToField'
@@ -23,34 +18,6 @@ import './style/playerCards.scss';
 import './style/constants.scss';
 
 initWindowCards();
-diceInit();
 
 startWindow();
-
-
-// export const player = new Player('#2295FF');
-// Game.addPlayer(player);
-// Game.activePlayer = player;
-// Game.activePlayer.position = 6;
-// for (let i = 0; i < Game.cells.length; i += 1) {
-//   if (Game.cells[i].name === 'Vermont Avenue' || Game.cells[i].name === 'Connecticut Avenue') {
-//     player.addProperty(Game.cells[i]);
-//   }
-// }
-// showDialogWindow('roll');
-
-// const player1 = new Player('#2295FF', 'player1');
-// const player2 = new Player('#2295FF', 'player2');
-// Game.addPlayer(player1);
-// Game.addPlayer(player2);
-
-// renderPlayerCard();
-// addPlayerToField();
-
-// Game.activePlayer = player1;
-// console.log(Game);
-// setNextPlayerAsActive();
-// console.log(Game);
-// setNextPlayerAsActive();
-// console.log(Game);
-// setNextPlayerAsActive();
+roll();
