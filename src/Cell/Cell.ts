5b6ae0de8d9c02ba07f1a866bcb450107b7d81f7
Sergey Danchenko <s.danchienko@mail.ru>
2021-01-25 13:09:58 +0300

    refactor: rewrite Game.js and Cell.js to typescript

---
+++ src/Cell/Cell.ts
@@ -0,0 +1,13 @@
+export default class Cell {
+  readonly type: string;
+  readonly name: string;
+  readonly element: HTMLElement;
+  position: number;
+
+  constructor(type: string, name: string, position: number) {
+    this.type = type;
+    this.name = name;
+    this.position = position;
+    this.element = document.getElementById(`${position}`);
+  }
+}
