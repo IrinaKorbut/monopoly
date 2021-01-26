88330a851b9b63fa2cb1bf484d066d47f9b279b9
IrinaKorbut <irisharomka96@gmail.com>
2021-01-26 21:19:57 +0300

    refactor: rewrite js to ts

--- src/histiryWindow/historyWindow.ts
+++ src/histiryWindow/historyWindow.ts
@@ -1,8 +1,8 @@
 import { createElement, appendElementTo } from '../helpFunctions/helpFunctions';
 import Game from '../Game/Game';
 
-export default function initHistoryWindow(action) {
-  const list = document.querySelector('.action-list');
+export default function initHistoryWindow(action: string) {
+  const list: HTMLElement = document.querySelector('.action-list');
   appendElementTo(list, createElement('li', ['action'], `${Game.activePlayer.name}: ${action}`));
   list.scrollTop = list.scrollHeight;
 }
