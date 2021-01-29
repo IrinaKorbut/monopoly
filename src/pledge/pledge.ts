71055182b30c8112ab605b1cca9e99307f064ac4
Sergey Danchenko <s.danchienko@mail.ru>
2021-01-29 17:50:37 +0300

    feat: add languages to pledge button

--- src/pledge/pledge.ts
+++ src/pledge/pledge.ts
@@ -3,6 +3,40 @@ import Game from '../Game/Game';
 import { appendElementTo, createElement } from '../helpFunctions/helpFunctions';
 import Property from '../ifacies/Property';
 
+
+function setPledgeBtnText() {
+  const language: string = localStorage.getItem('language');
+  const pladgeBtn: HTMLElement = document.querySelector('.button__pladge');
+    if (language === 'RU') {
+      pladgeBtn.innerText = 'Залог';
+    } else if (language === 'BEL') {
+      pladgeBtn.innerText = 'Заклад';
+    } else {
+      pladgeBtn.innerText = 'Pledge';
+    }
+}
+
+function setFinishPledgeBtnText() {
+  const language: string = localStorage.getItem('language');
+  const pladgeBtn: HTMLElement = document.querySelector('.button__pladge');
+  if (language === 'RU') {
+    pladgeBtn.innerText = 'Остановить залог';
+  } else if (language === 'BEL') {
+    pladgeBtn.innerText = 'Спыніць заклад';
+  } else {
+    pladgeBtn.innerText = 'Finish pledge';
+  }
+}
+
+export function changePledgeBtnLanguage() {
+  const pladgeBtn: HTMLElement = document.querySelector('.button__pladge');
+  if (pladgeBtn.innerText === 'Pledge' || pladgeBtn.innerText === 'Залог' || pladgeBtn.innerText === 'Заклад') {
+    setPledgeBtnText();
+  } else {
+    setFinishPledgeBtnText();
+  }
+}
+
 function pledgeProperty(event: any): void {
   const cellElement: HTMLElement = event.target.parentNode;
   let currentObjCell: Property;
@@ -23,7 +57,7 @@ function pledgeProperty(event: any): void {
 }
 
 function pledgeBtnEvent(event: any): void {
-  if (event.target.innerText === 'Pledge') {
+  if (event.target.innerText === 'Pledge' || event.target.innerText === 'Залог' || event.target.innerText === 'Заклад') {
     Game.cells.forEach((cell: Property) => {
       if (Game.activePlayer.property.includes(cell) && !cell.isPredge && (cell.numberOfHouses === 0 || cell.numberOfHouses === undefined)) {
         cell.element.addEventListener('click', pledgeProperty);
@@ -31,9 +65,10 @@ function pledgeBtnEvent(event: any): void {
         cell.element.querySelector('.players-container').classList.add('dark');
       }
     });
-    event.target.innerText = 'Finish pledge';
+    const language: string = localStorage.getItem('language');
+    setFinishPledgeBtnText();
   } else {
-    event.target.innerText = 'Pledge';
+    setPledgeBtnText();
     Game.cells.forEach((cell: Property) => {
       cell.element.querySelector('.players-container').classList.remove('dark');
       cell.element.removeEventListener('click', pledgeProperty);
@@ -43,7 +78,17 @@ function pledgeBtnEvent(event: any): void {
 
 export default function initPledgeBtn(): void {
   const buyingSection: HTMLElement = document.querySelector('.buying-section');
-  const pledgeBtn: HTMLElement = createElement('div', ['button__buy-house'], 'Pledge');
+  let btnInnerText: string; 
+  const language: string = localStorage.getItem('language');
+  if (language === 'RU') {
+    btnInnerText = 'Залог';
+  } else if (language === 'BEL') {
+    btnInnerText = 'Заклад';
+  } else {
+    btnInnerText = 'Pledge';
+  }
+  const pledgeBtn: HTMLElement = createElement('div', ['button__buy-house', 'button__pladge'], btnInnerText);
   pledgeBtn.addEventListener('click', pledgeBtnEvent);
   appendElementTo(buyingSection, pledgeBtn);
 }
+
