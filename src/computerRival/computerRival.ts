1f40ed16d910b03ef1ecb13b42132024b47feb32
Sergey Danchenko <s.danchienko@mail.ru>
2021-01-31 17:21:16 +0300

    feat: translate computer actions

--- src/computerRival/computerRival.ts
+++ src/computerRival/computerRival.ts
@@ -8,6 +8,7 @@ import { checkIsHuman } from '../buyHouse/buyHouse';
 import initHistoryWindow from '../histiryWindow/historyWindow';
 
 export default function computerMove(action?: string): void {
+  const language = localStorage.getItem('language');
   const cell = getCellObjByPosition(game.activePlayer.position);
   switch (action) {
     case 'roll':
@@ -25,7 +26,13 @@ export default function computerMove(action?: string): void {
       p.then(() => {
         const diceValue = roll();
         movePlayer(diceValue);
-        initHistoryWindow(`rolled ${diceValue} on the dice`);
+        if (language === 'RU') {
+          initHistoryWindow(`выкинул(а) ${diceValue} на кубиках`);
+        } else if (language === 'BEL') {
+          initHistoryWindow(`выкінуў(ла) ${diceValue} на кубіках`);
+        } else {
+          initHistoryWindow(`rolled ${diceValue} on the dice`);
+        }
       });
       break;
     case 'buy':
@@ -41,8 +48,13 @@ export default function computerMove(action?: string): void {
         } else {
           setCommunalRent(cell, game.activePlayer);
         }
-        // логика постройки дома
-        initHistoryWindow(`bought ${cell.name} for $${cell.cost}`);
+        if (language === 'RU') {
+          initHistoryWindow(`купил(ла) ${cell.russianName} за $${cell.cost}`);
+        } else if (language === 'BEL') {
+          initHistoryWindow(`купіў(ла) ${cell.belarusianName} за $${cell.cost}`);
+        } else {
+          initHistoryWindow(`bought ${cell.name} for $${cell.cost}`);
+        }
         computerMove();
       } else {
         computerMove();
@@ -55,7 +67,13 @@ export default function computerMove(action?: string): void {
           changeMoneyOnPlayerCard(game.activePlayer);
           cell.owner.addMoney(cell.currentRent);
           changeMoneyOnPlayerCard(cell.owner);
-          initHistoryWindow(`paid $${cell.currentRent} rent to ${cell.owner.name}`);
+          if (language === 'RU') {
+            initHistoryWindow(`заплатил(а) $${cell.currentRent} аренды ${cell.owner.name}`);
+          } else if (language === 'BEL') {
+            initHistoryWindow(`заплаціў(ла) $${cell.currentRent} арэнды ${cell.owner.name}`);
+          } else {
+            initHistoryWindow(`paid $${cell.currentRent} rent to ${cell.owner.name}`);
+          }
           computerMove();
         } else {
           // доработать
@@ -80,7 +98,13 @@ export default function computerMove(action?: string): void {
             changeMoneyOnPlayerCard(game.activePlayer);
             cell.owner.addMoney(rent);
             changeMoneyOnPlayerCard(cell.owner);
-            initHistoryWindow(`paid $${rent} rent to ${cell.owner.name}`);
+            if (language === 'RU') {
+              initHistoryWindow(`заплатил(а) $${cell.currentRent} аренды ${cell.owner.name}`);
+            } else if (language === 'BEL') {
+              initHistoryWindow(`заплаціў(ла) $${cell.currentRent} арэнды ${cell.owner.name}`);
+            } else {
+              initHistoryWindow(`paid $${cell.currentRent} rent to ${cell.owner.name}`);
+            }
             computerMove();
           } else {
             // доработать
@@ -93,7 +117,13 @@ export default function computerMove(action?: string): void {
       if (isPlayerHaveEnoughMoney(game.activePlayer, cell.cost)) {
         game.activePlayer.subtractMoney(cell.cost);
         changeMoneyOnPlayerCard(game.activePlayer);
-        initHistoryWindow(`paid $${cell.cost} ${cell.name}`);
+        if (language === 'RU') {
+          initHistoryWindow(`заплатил(а) $${cell.cost} ${cell.russianName}`);
+        } else if (language === 'BEL') {
+          initHistoryWindow(`заплаціў(ла) $${cell.cost} ${cell.belarusianName}`);
+        } else {
+          initHistoryWindow(`paid $${cell.cost} ${cell.name}`);
+        }
         computerMove();
       } else {
         // доработать
